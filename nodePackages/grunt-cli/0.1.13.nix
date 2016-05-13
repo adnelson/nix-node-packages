9c8a4ce0b0fc3363de79625dc6e69841ad62a99f
Allen Nelson <anelson@narrativescience.com>
2016-05-13 19:36:37 +0000

    patch grunt-cli to just require grunt the normal way

--- nodePackages/grunt-cli/0.1.13.nix
+++ nodePackages/grunt-cli/0.1.13.nix
@@ -6,10 +6,14 @@ buildNodePackage {
       url = "http://registry.npmjs.org/grunt-cli/-/grunt-cli-0.1.13.tgz";
       sha1 = "e9ebc4047631f5012d922770c39378133cad10f4";
     };
+    postPatch = ''
+      patch -p0 -i ${./use_regular_grunt.patch}
+    '';
     deps = with nodePackages; [
       nopt_1-0-10
       findup-sync_0-1-3
       resolve_0-3-1
+      grunt_0-4-5
     ];
     meta = {
       homepage = "http://gruntjs.com/";
