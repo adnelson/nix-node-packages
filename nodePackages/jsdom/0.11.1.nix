c90f46d5eb05070ea8ef12e9c3e87e38f35d5e7b
Allen Nelson <anelson@narrativescience.com>
2016-09-08 19:00:46 +0000

    fix jsdom 0.11.1

--- nodePackages/jsdom/0.11.1.nix
+++ nodePackages/jsdom/0.11.1.nix
@@ -6,6 +6,12 @@ buildNodePackage {
       url = "http://registry.npmjs.org/jsdom/-/jsdom-0.11.1.tgz";
       sha1 = "f1a79756ebc2116932caef8c6bfde7022dacdbfb";
     };
+    # Apply a patch to a known bug in old versions of jsdom. This fix
+    # can be found in this commit:
+    # https://github.com/tmpvar/jsdom/commit/ade9529f45ce4436dbc0447d31e13beab6b72b7d
+    postPatch = ''
+      patch -p0 -i ${./set-length.patch}
+    '';
     deps = with nodePackages; [
       xmlhttprequest_1-8-0
       htmlparser2_3-9-0
