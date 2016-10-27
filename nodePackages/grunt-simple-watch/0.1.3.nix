a869c5088418e89bcada36dc6aa3cfae8a5a0c36
Allen Nelson <anelson@narrativescience.com>
2016-10-27 20:13:40 +0000

    fix bug with incorrect entry point in grunt-simple-watch

--- nodePackages/grunt-simple-watch/0.1.3.nix
+++ nodePackages/grunt-simple-watch/0.1.3.nix
@@ -7,6 +7,16 @@ buildNodePackage {
       sha1 = "9224707afba3dc78961ecc41193405b9655988b7";
     };
     deps = [];
+    postPatch = ''
+      python <<PYTHON
+      import json
+      with open('package.json') as f:
+          pkg_json = json.load(f)
+      pkg_json['main'] = 'tasks/simple_watch.js'
+      with open('package.json', 'w') as f:
+          f.write(json.dumps(pkg_json, indent=2))
+      PYTHON
+    '';
     meta = {
       homepage = "https://github.com/unbalanced/grunt-simple-watch";
       description = "no need to change your gruntfile configurations";
