7cdddf9f4a8b79fb86191524cb695dc043cd937e
Salt7900 <bnfallon@gmail.com>
2017-12-05 11:34:17 -0600

    fix spacing
    add patch phase

--- nodePackages/@types/jasmine/2.5.54.nix
+++ nodePackages/@types/jasmine/2.5.54.nix
@@ -3,9 +3,10 @@ buildNodePackage {
     name = "jasmine";
     version = "2.5.54";
     src = fetchUrlWithHeaders {
-    url = "https://registry.npmjs.org/@types/jasmine/-/jasmine-2.5.54.tgz";
+      url = "https://registry.npmjs.org/@types/jasmine/-/jasmine-2.5.54.tgz";
       sha1 = "a6b5f2ae2afb6e0307774e8c7c608e037d491c63";
     };
+    patchPhase = "touch index.js";
     namespace = "types";
     deps = [];
     devDependencies = [];
